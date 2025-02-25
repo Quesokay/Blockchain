cd65d6426cf431248e70c8bdc081ff1100b19d07
Victor Moyo <systems@shearwatervf.com>
2025-02-25 19:27:34 +0200

    first commit

---
+++ dev/test.js
@@ -0,0 +1,34 @@
+const BlockChain = require('./Blockchain');
+const bitcoin = new BlockChain();
+//testing genesis block
+console.log(bitcoin);
+// bitcoin.createNewBlock(2389,'OINA90SDNF90N','90ANSD9F0N9009N');
+// bitcoin.createNewTransaction(100,'QUESD89F9W0N90A','NYAN0FDHGSH8');
+// bitcoin.createNewBlock(5422,'OINA90SDNF90N','90ANSD9F0N9009N');
+// bitcoin.createNewTransaction(50,'QUESD89F9W0N90A','NYAN0FDHGSH8');
+// bitcoin.createNewTransaction(300,'QUESD89F9W0N90A','NYAN0FDHGSH8');
+// bitcoin.createNewTransaction(2000,'QUESD89F9W0N90A','NYAN0FDHGSH8');
+// bitcoin.createNewBlock(4546,'OINA90SDNF90N','90ANSD9F0N9009N');
+const previousBlockHash = 'FDWDV453HSDH343JHJB';
+const currentBlockData = [
+        {
+            amount:10,
+            sender:'N90JSHFND',
+            recipient:'MNMD45JDHD'
+        },
+        {
+            amount:0.5,
+            sender:'NERRG90JSHFND',
+            recipient:'HGS77GG'
+        },
+        {
+            amount:20,
+            sender:'RTFN90JSHDSFND',
+            recipient:'Mhee384hdh'
+        },
+];
+// const nonce = 100;
+// console.log(bitcoin.hashBlock(previousBlockHash,currentBlockData,nonce));
+//testing proofOfWork function.
+// console.log(bitcoin.proofOfWork(previousBlockHash,currentBlockData));
+console.log(bitcoin.hashBlock(previousBlockHash,currentBlockData,27470));
