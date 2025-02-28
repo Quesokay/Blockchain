8b487ad76eaab576971cb8e0d96dc9793e35a01a
Victor Moyo <systems@shearwatervf.com>
2025-02-28 16:03:55 +0200

    Add initial nodemon and semver files with configuration

--- dev/api.js
+++ dev/api.js
@@ -1,21 +1,38 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const bodyParser = require('body-parser')
+// const express = require('express')
+// const app = express()
+// const port = 3000
 
-app.use(bodyParser.json)
-app.use(bodyParser.urlencoded({extended: false}))
+// const BlockChain = require('./Blockchain');
 
-app.get('/blockchain', (req, res) => {
+// const bitcoin = new BlockChain();
+
+
+
+// app.get('/blockchain', (req, res) => {
+//   res.send(bitcoin);
+// });
+
+
+
+// app.get('/mine',function(req,res){
+
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+app.use(bodyParser.json);
+app.use(bodyParser.urlencoded({extended: false}));
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 });
-
 app.post('/transaction', function(req,res){
   console.log(req.body)
   res.send(`The amount of the transaction is ${req.body.amount} bitcoin`);
-});
-
-app.get('/mine',function(req,res){
-
 });
 
 app.listen(port, () => {
