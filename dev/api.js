7a6ecc9e3fb87bb7db6aebdeee35291266e60233
Victor Moyo <systems@shearwatervf.com>
2025-02-28 16:02:52 +0200

    Add nodeman module with initial documentation and configuration

--- dev/api.js
+++ dev/api.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(bodyParser.json)
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.get('/blockchain', (req, res) => {
+});
+
+app.post('/transaction', function(req,res){
+  console.log(req.body)
+  res.send(`The amount of the transaction is ${req.body.amount} bitcoin`);
+});
+
+app.get('/mine',function(req,res){
+
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
